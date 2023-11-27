8d34b6397d855ef0a77f4649499e4092782c5cb8
Lien Hung Trinh <thuyhung2112@yahoo.com>
2023-11-27 15:59:49 +0700

    tách code thành 2 class riêng (cùng namespace)

---
+++ StackCalcForm/CalcCtrls.cs
@@ -0,0 +1,137 @@
+﻿namespace StackCalcForm
+{
+    class CalcCtrls
+    {
+        private readonly IStackCalc _form;
+        public CalcCtrls(IStackCalc form)
+        {
+            _form = form;
+        }
+
+        public void AddOpenParen()
+        {
+            if ((_form.Result == "0" || _form.Result == string.Empty)
+                && !_form.Input.ToString().EndsWith(')'))
+            {
+                _form.Input += "(";
+                Form1.InputAdds.Add("(");
+            }
+            else if (_form.Input.ToString().EndsWith(')'))
+            {
+                _form.Input += "*(";
+                Form1.InputAdds.Add("*");
+                Form1.InputAdds.Add("(");
+            }
+            else
+            {
+                _form.Input += _form.Result + "*(";
+                Form1.InputAdds.Add(_form.Result.ToString());
+                Form1.InputAdds.Add("*");
+                Form1.InputAdds.Add("(");
+            }
+            _form.Result = "0";
+        }
+
+        public void AddCloseParen()
+        {
+            if (_form.Input.ToString().EndsWith('('))
+            {
+                _form.Input += _form.Result + ")";
+                Form1.InputAdds.Add(_form.Result.ToString());
+                Form1.InputAdds.Add(")");
+            }
+            else
+            {
+                string inputStr = _form.Input.ToString();
+                if (inputStr.Count(c => c == '(') > inputStr.Count(c => c == ')'))
+                {
+                    _form.Input += _form.Result + ")";
+                    Form1.InputAdds.Add(_form.Result.ToString());
+                    Form1.InputAdds.Add(")");
+                }
+            }
+            _form.Result = "0";
+        }
+
+        public void AddOperation(string c)
+        {
+            string inputStr = _form.Input.ToString();
+            List<string> oprList = new List<string>() { "+", "-", "*", "/", "^" };
+            if (oprList.Any(c => inputStr.EndsWith(c)) && _form.Result == "0")
+            {
+                _form.Input = inputStr.Remove(inputStr.Length - 1);
+                _form.Input += c;
+                Form1.InputAdds.RemoveAt(Form1.InputAdds.Count - 1);
+                Form1.InputAdds.Add(c);
+            }
+            else if (inputStr.EndsWith(')'))
+            {
+                _form.Input += c;
+                Form1.InputAdds.Add(c);
+            }
+            else
+            {
+                _form.Input += _form.Result + c;
+                Form1.InputAdds.Add(_form.Result.ToString());
+                Form1.InputAdds.Add(c);
+            }
+            _form.Result = "0";
+        }
+
+        public void AddDigit(string n)
+        {
+            if (n == "0" && _form.Result != "0")
+            {
+                _form.Result += "0";
+            }
+            else if (_form.Result == "0")
+            {
+                _form.Result = n;
+            }
+            else
+            {
+                _form.Result += n;
+            }
+        }
+
+        public void Evaluate()
+        {
+            string inputStr = _form.Input.ToString();
+            if (inputStr.Count(c => c == '(') > inputStr.Count(c => c == ')'))
+            {
+                _form.Input += _form.Result + ")";
+                Form1.InputAdds.Add(_form.Result.ToString());
+                Form1.InputAdds.Add(")");
+            }
+            else if (!inputStr.EndsWith(')'))
+            {
+                _form.Input += _form.Result;
+                Form1.InputAdds.Add(_form.Result.ToString());
+            }
+            string postFix = CalcProcess.ProcessExpression();
+            double result = CalcProcess.CalculateFromPostfix(postFix);
+            Form1.InputAdds.Clear(); _form.Input = string.Empty;
+            if (double.IsNegativeInfinity(result))
+            {
+                _form.Result = "Error";
+            }
+            else
+            {
+                _form.Result = result.ToString();
+            }
+        }
+
+        public void Backspace()
+        {
+            if (_form.Result.Length == 1)
+            {
+                _form.Result = "0";
+            }
+            else
+            {
+                string inputNumStr = _form.Result.ToString();
+                _form.Result = inputNumStr[..^1];
+            }
+        }
+    }
+}
---
+++ StackCalcForm/CalcProcess.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+
+namespace StackCalcForm
+{
+    class CalcProcess
+    {
+        private static int Precedence(string? x)
+        {
+            if (x == "+" || x == "-") return 1;
+            else if (x == "*" || x == "/") return 2;
+            else if (x == "^") return 3;
+            else return -1;
+        }
+
+        public static string ProcessExpression()
+        {
+            Stack addStack = new Stack();
+            string postFix = string.Empty;
+            string[] arrInAdds = Form1.InputAdds.ToArray();
+            for (int i = 0; i < arrInAdds.Length; ++i)
+            {
+                string opr = arrInAdds[i];
+                if (double.TryParse(opr, out double num))
+                {
+                    postFix += num + " ";
+                }
+                else if (opr == "(")
+                {
+                    addStack.Push(opr);
+                }
+                else if (opr == ")")
+                {
+                    while (addStack.Count > 0 && addStack.Peek()?.ToString() != "(")
+                    {
+                        postFix += addStack.Pop()?.ToString() + " ";
+                    }
+                    if (addStack.Count > 0 && addStack.Peek()?.ToString() != "(")
+                    {
+                        return "Error";
+                    }
+                    else if (addStack.Count > 0)
+                    {
+                        addStack.Pop();
+                    }
+                }
+                else
+                {
+                    while (addStack.Count > 0
+                        && Precedence(opr) <= Precedence(addStack.Peek()?.ToString()))
+                    {
+                        postFix += addStack.Pop()?.ToString() + " ";
+                    }
+                    addStack.Push(opr);
+                }
+            }
+            while (addStack.Count > 0)
+            {
+                postFix += addStack.Pop()?.ToString() + " ";
+            }
+            return postFix;
+        }
+
+        public static double CalculateFromPostfix(string postFix)
+        {
+            Stack addStack = new Stack();
+            string[] adds = postFix.Split(' ');
+            bool isValid = true;
+            for (int i = 0; i < adds.Length; i++)
+            {
+                if (double.TryParse(adds[i], out double num))
+                {
+                    addStack.Push(num);
+                }
+                else
+                {
+                    string opr = adds[i];
+                    if (addStack.Count > 1)
+                    {
+                        double a = Convert.ToDouble(addStack.Pop());
+                        double b = Convert.ToDouble(addStack.Pop());
+                        switch (opr)
+                        {
+                            case "+": addStack.Push(b + a); break;
+                            case "-": addStack.Push(b - a); break;
+                            case "*": addStack.Push(b * a); break;
+                            case "/": addStack.Push(b / a); break;
+                            case "^": addStack.Push(Math.Pow(b, a)); break;
+                            default: isValid = false; break;
+                        }
+                    }
+                    else if (addStack.Count == 1)
+                    {
+                        if (opr == "-")
+                        {
+                            double x = Convert.ToDouble(addStack.Pop());
+                            addStack.Push(-x);
+                        }
+                    }
+                    else
+                    {
+                        isValid = false;
+                    }
+                }
+            }
+            if (!isValid)
+            {
+                return double.NegativeInfinity;
+            }
+            double result = Convert.ToDouble(addStack.Pop());
+            return result;
+        }
+    }
+}
