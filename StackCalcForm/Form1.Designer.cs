ca1f8a863a4e09ffbdff28b2f9897cfbf8fac410
Lien Hung Trinh <thuyhung2112@yahoo.com>
2023-11-27 16:02:40 +0700

    giản lược, chuyển một phần code sang các file khác

--- StackCalcForm/Form1.Designer.cs
+++ StackCalcForm/Form1.Designer.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-
-namespace StackCalcForm
+﻿namespace StackCalcForm
 {
     partial class Form1
     {
@@ -8,8 +6,6 @@ namespace StackCalcForm
         ///  Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
-        private List<string> inAdds = new List<string>();
-        private Stack addStack = new Stack();
 
         /// <summary>
         ///  Clean up any resources being used.
@@ -22,110 +18,6 @@ namespace StackCalcForm
                 components.Dispose();
             }
             base.Dispose(disposing);
-        }
-
-        private static int Precedence(string x)
-        {
-            if (x == "+" || x == "-") return 1;
-            else if (x == "*" || x == "/") return 2;
-            else if (x == "^") return 3;
-            else return -1;
-        }
-
-        private string ProcessExpression()
-        {
-            string postFix = string.Empty;
-            string[] arrInAdds = inAdds.ToArray();
-            for (int i = 0; i < arrInAdds.Length; ++i)
-            {
-                string opr = arrInAdds[i];
-                if (double.TryParse(opr, out double num))
-                {
-                    postFix += num + " ";
-                }
-                else if (opr == "(")
-                {
-                    addStack.Push(opr);
-                }
-                else if (opr == ")")
-                {
-                    while (addStack.Count > 0 && addStack.Peek().ToString() != "(")
-                    {
-                        postFix += addStack.Pop().ToString() + " ";
-                    }
-                    if (addStack.Count > 0 && addStack.Peek().ToString() != "(")
-                    {
-                        return "Error";
-                    }
-                    else if (addStack.Count > 0)
-                    {
-                        addStack.Pop();
-                    }
-                }
-                else
-                {
-                    while (addStack.Count > 0
-                        && Precedence(opr) <= Precedence(addStack.Peek().ToString()))
-                    {
-                        postFix += addStack.Pop().ToString() + " ";
-                    }
-                    addStack.Push(opr);
-                }
-            }
-            while (addStack.Count > 0)
-            {
-                postFix += addStack.Pop().ToString() + " ";
-            }
-            return postFix;
-        }
-
-        private double CalculateFromPostfix(string postFix)
-        {
-            string[] adds = postFix.Split(' ');
-            bool isValid = true;
-            for (int i = 0; i < adds.Length; i++)
-            {
-                if (double.TryParse(adds[i], out double num))
-                {
-                    addStack.Push(num);
-                }
-                else
-                {
-                    string opr = adds[i];
-                    if (addStack.Count > 1)
-                    {
-                        double a = Convert.ToDouble(addStack.Pop());
-                        double b = Convert.ToDouble(addStack.Pop());
-                        switch (opr)
-                        {
-                            case "+": addStack.Push(b + a); break;
-                            case "-": addStack.Push(b - a); break;
-                            case "*": addStack.Push(b * a); break;
-                            case "/": addStack.Push(b / a); break;
-                            case "^": addStack.Push(Math.Pow(b, a)); break;
-                            default: isValid = false; break;
-                        }
-                    }
-                    else if (addStack.Count == 1)
-                    {
-                        if (opr == "-")
-                        {
-                            double x = Convert.ToDouble(addStack.Pop());
-                            addStack.Push(-x);
-                        }
-                    }
-                    else
-                    {
-                        isValid = false;
-                    }
-                }
-            }
-            if (!isValid)
-            {
-                return double.NegativeInfinity;
-            }
-            double result = Convert.ToDouble(addStack.Pop());
-            return result;
         }
 
         #region Windows Form Designer generated code
