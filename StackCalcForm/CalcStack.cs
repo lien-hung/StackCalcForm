794e598f6d17a8357ae6e7b0b233b404079bb757
Lien Hung Trinh <thuyhung2112@yahoo.com>
2023-12-06 09:03:40 +0700

    Thêm class chứa cấu trúc Stack tự tạo

---
+++ StackCalcForm/CalcStack.cs
@@ -0,0 +1,40 @@
+﻿namespace StackCalcForm
+{
+    class CalcStack<T>
+    {
+        private readonly T[] items;
+        private int top;
+        private readonly int capacity;
+        public int Count;
+        public CalcStack(int size)
+        {
+            capacity = size;
+            items = new T[capacity];
+            top = -1;
+            Count = 0;
+        }
+        public void Push(T item)
+        {
+            items[++top] = item;
+            Count++;
+        }
+        public T? Pop()
+        {
+            if (top == -1)
+            {
+                return default;
+            }
+            T poppedItem = items[top--];
+            Count--;
+            return poppedItem;
+        }
+        public T Peek()
+        {
+            return items[top];
+        }
+        public bool IsEmpty()
+        {
+            return top == -1;
+        }
+    }
+}
